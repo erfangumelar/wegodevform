1991fc45681355bbb585398a6642bb02bd1d9ad6
erfangumelar <erfangumelar@gmail.com>
2022-10-21 13:03:34 +0700

    add middleware jwt

---
+++ backend/middlewares/jwtAuth.js
@@ -0,0 +1,40 @@
+import jsonwebtoken, { decode } from "jsonwebtoken";
+import dotenv from "dotenv";
+
+const env = dotenv.config().parsed;
+
+const jwtAuth = () => {
+  return async (req, res, next) => {
+    try {
+      if (!req.headers.authorization) {
+        throw { code: 401, message: "UNAUTHORIZED" };
+      }
+
+      const token = req.headers.authorization.split(" ")[1]; // Bearer <token>
+      const verify = jsonwebtoken.verify(token, env.JWT_ACCESS_TOKEN_SECRET);
+      req.jwt = verify; // sekarang kita punya req.jwt dari data user yang login
+
+      next(); // Continue to next another middleware / Controller
+    } catch (error) {
+      const errorJwt = [
+        "invalid signature",
+        "jwt malformed",
+        "jwt must be provided",
+        "invalid token",
+      ];
+
+      if (error.message == "jwt expired") {
+        error.message = "ACCESS_TOKEN_EXPIRED";
+      } else if (errorJwt.includes(error.message)) {
+        error.message = "INVALID_ACCESS_TOKEN";
+      }
+
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  };
+};
+
+export default jwtAuth;
