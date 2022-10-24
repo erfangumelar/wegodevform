38ced5fc2b95666f6d421319c9d639d4c6bb3aaf
erfangumelar <erfangumelar@gmail.com>
2022-10-24 14:01:11 +0700

    add show form

--- backend/controllers/FormController.js
+++ backend/controllers/FormController.js
@@ -27,6 +27,39 @@ class FormController {
       });
     }
   }
+
+  async show(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 404, message: "REQUIRED_FORM_ID" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "INVALID_ID" };
+      }
+
+      // mengecek form ke database
+      const form = await Form.findOne({
+        _id: req.params.id,
+        userId: req.jwt.id,
+      });
+
+      // mengecek apakah form ada / tidak
+      if (!form) {
+        throw { code: 400, message: "FORM_NOT_FOUND" };
+      }
+
+      return res.status(200).json({
+        status: true,
+        message: "FORM_FOUND",
+        form,
+      });
+    } catch (error) {
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
 }
 
 export default new FormController();
