b841ecc88cdb398ce99a9aedbc2041146ce87921
erfangumelar <erfangumelar@gmail.com>
2022-10-24 15:57:42 +0700

    add update data form

--- backend/controllers/FormController.js
+++ backend/controllers/FormController.js
@@ -60,6 +60,35 @@ class FormController {
       });
     }
   }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 404, message: "REQUIRED_FORM_ID" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "INVALID_ID" };
+      }
+
+      const form = await Form.findOneAndUpdate(
+        { _id: req.params.id, userId: req.jwt.id },
+        req.body,
+        { new: true }
+      );
+      if (!form) throw { code: 404, message: "FORM_UPDATE_FAILED" };
+
+      return res.status(200).json({
+        status: true,
+        message: "FORM_UPDATE_SUCCESS",
+        form,
+      });
+    } catch (error) {
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
 }
 
 export default new FormController();
