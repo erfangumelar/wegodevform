0612cf48c53947ea83fda182d20864d9e7495256
erfangumelar <erfangumelar@gmail.com>
2022-10-28 14:09:13 +0700

    add store new questions

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,7 @@ import express from "express";
 import jwtAuth from "../middlewares/jwtAuth.js";
 import AuthController from "../controllers/AuthController.js";
 import FormController from "../controllers/FormController.js";
+import QuestionController from "../controllers/QuestionController.js";
 
 const router = express.Router();
 
@@ -16,5 +17,12 @@ router.post("/forms", jwtAuth(), FormController.store);
 router.get("/forms/:id", jwtAuth(), FormController.show);
 router.put("/forms/:id", jwtAuth(), FormController.update);
 router.delete("/forms/:id", jwtAuth(), FormController.destroy);
+
+// Question
+// router.get("/forms", jwtAuth(), FormController.index);
+router.post("/forms/:id/questions", jwtAuth(), QuestionController.store);
+// router.get("/forms/:id", jwtAuth(), FormController.show);
+// router.put("/forms/:id", jwtAuth(), FormController.update);
+// router.delete("/forms/:id", jwtAuth(), FormController.destroy);
 
 export default router;
